7597afbf380d017e232c9c15ae98a336352553b3
Cormac Pujals <cmpujals@gmail.com>
2022-12-19 13:01:48 -0800

    style for detail page and begins profile model creation

--- src/pages/ParkDetail/ParkDetail.jsx
+++ src/pages/ParkDetail/ParkDetail.jsx
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom";
+import "./ParkDetail.css"
 
 export default function ParkDetail({parks}) {
   const { parkId } = useParams();
   const park = parks.find(p => p.id === parkId) 
   return (
-    <>
-      <h1>Park: {park.name}</h1>
-    </>
+    <div className="parkDetail">
+      <h1>{park.name}</h1>
+      <img src={park.images[0].url} alt=""/>
+      <br />
+      <br />
+      <a href={park.url} target="_blank" >Find out more!</a>
+      <p>{park.description}</p>
+      <h6>Latitude: {park.latitude} Longitude: {park.longitude}</h6>
+      <a href={park.directionsUrl} target="_blank" >Get directions here!</a>
+    </div>
   )
 }
